6333133146aea2ab21ad253e491581a01237c4b9
naol <naolatechshd@gmail.com>
2024-06-05 23:10:51 -0700

    modification

--- load_dataset.py
+++ load_dataset.py
@@ -3,7 +3,3 @@ import pandas as pd
 def load_data(file_path):
     return pd.read_csv(file_path)
 
-
-if __name__=='__main__':
-    path='C:/Users/user/Desktop/EPHI-Challenge/data/CHAMPS.csv'
-    load_data(path)
